6c0b10b3a56cbd34fc5369eadfeaf9eabe05dec9
KAJAL <kajalangural1@gmail.com>
2017-07-17 12:54:21 -0700

    signup_view and login_view generated

---
+++ mysite/myapp/views.py
@@ -0,0 +1,59 @@
+
+from django.shortcuts import render, redirect
+from forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
+from models import User, SessionToken, PostModel, LikeModel, CommentModel
+from django.contrib.auth.hashers import make_password, check_password
+from datetime import timedelta
+from django.utils import timezone
+from mysite.settings import BASE_DIR
+
+from imgurpython import ImgurClient
+
+# Create your views here.
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            #saving data to DB
+            user = User(name=name, password=make_password(password), email=email, username=username)
+            user.save()
+            return render(request, 'feed.html')
+            #return redirect('login/')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'signup.html', {'form' : form})
+
+
+def login_view(request):
+    response_data = {}
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = User.objects.filter(username=username).first()
+
+            if user:
+                if check_password(password, user.password):
+                    token = SessionToken(user=user)
+                    token.create_token()
+                    token.save()
+                    response = redirect('feed/')
+                    response.set_cookie(key='session_token', value=token.session_token)
+                    return response
+                else:
+                    response_data['message'] = 'Incorrect Password! Please try again!'
+
+    elif request.method == 'GET':
+        form = LoginForm()
+
+    response_data['form'] = form
+    return render(request, 'login1.html', response_data)
+
+
