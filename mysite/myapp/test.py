6be8e29df35ac9cfd18daeb7e1d59cd97816a42d
KAJAL <kajalangural1@gmail.com>
2017-07-19 18:18:29 -0700

    like_view and comment_view generated

--- mysite/myapp/test.py
+++ mysite/myapp/test.py
@@ -1,3 +1,7 @@
 from clarifai.rest import ClarifaiApp
 
 app = ClarifaiApp(api_key='a5f07c4b20b84ff8a66790faedaf9314')
+
+model = app.models.get('general-v1.3')
+image = ClImage(url='https://samples.clarifai.com/metro-north.jpg')
+model.predict([image])
